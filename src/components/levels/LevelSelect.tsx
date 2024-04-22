029cc393de52e4f8f786e73d2f14b3ce0a374af2
Caleb Wei <cwei@ict.co>
2024-04-23 00:43:23 +1200

    styled card and button components

--- src/components/levels/LevelSelect.tsx
+++ src/components/levels/LevelSelect.tsx
@@ -1,25 +1,25 @@
-import { HoverFolder } from '../ui/HoverFolder'
-import { Button } from '../ui/button'
-
+import { HoverFolder } from '../ui/HoverFolder';
+import { Button } from '../ui/button';
 
 interface LevelSelectProps {
-    handleLevelButtonClick: (level: number, event: React.MouseEvent<HTMLButtonElement>) => void;
-  }
+  handleLevelButtonClick: (
+    level: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+}
 
-export function LevelSelect({ handleLevelButtonClick }: LevelSelectProps){
-
-    return(
-        <div className="text-center flex flex-col items-center">
-            <HoverFolder overlap='0px' zIndex={0}/>
-            <HoverFolder overlap='-640px' zIndex={1}/>
-            <HoverFolder overlap='-640px'zIndex={2}/>
-            <Button
-            className="w-[20rem] h-[4rem] font-[BJG] mt-10 mb-10"
-            variant="outline"
-            onClick={(e) => handleLevelButtonClick(0, e)}
-            >
-            Back
-            </Button>
-        </div>
-    )
+export function LevelSelect({ handleLevelButtonClick }: LevelSelectProps) {
+  return (
+    <div className="text-center flex flex-col items-center">
+      <HoverFolder overlap="0px" zIndex={0} />
+      <HoverFolder overlap="-640px" zIndex={1} />
+      <HoverFolder overlap="-640px" zIndex={2} />
+      <Button
+        className="w-[20rem] h-[4rem] font-[BJG] mt-10 mb-10"
+        onClick={(e) => handleLevelButtonClick(0, e)}
+      >
+        Back
+      </Button>
+    </div>
+  );
 }
