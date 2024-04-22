57757f46a5f8e52c40a4875837fee251d03087ae
Caleb Wei <cwei@ict.co>
2024-04-23 01:03:34 +1200

    Changed initial and end of paper sprite animation to the bottom of display instead of a static value to fix visual bug.

--- src/components/levels/LevelSelect.tsx
+++ src/components/levels/LevelSelect.tsx
@@ -1,35 +1,37 @@
-import { HoverFolder } from '../ui/HoverFolder'
-import { Button } from '../ui/button'
+import { HoverFolder } from '../ui/HoverFolder';
+import { Button } from '../ui/button';
 import { useState } from 'react';
 
 interface LevelSelectProps {
-    handleLevelButtonClick: (level: number, event: React.MouseEvent<HTMLButtonElement>) => void;
-  }
+  handleLevelButtonClick: (
+    level: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+}
 
-export function LevelSelect({ handleLevelButtonClick }: LevelSelectProps){
+export function LevelSelect({ handleLevelButtonClick }: LevelSelectProps) {
+  const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
+  const [clickedIndex, setClickedIndex] = useState<number>(-1);
 
-    const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
-    const [clickedIndex, setClickedIndex] = useState<number>(-1);
-
-    return(
-        <div className="text-center flex flex-col items-center">
-            {['0px', '-650px', '-650px'].map((marginTop, index) => 
-                <HoverFolder 
-                marginTop={marginTop}
-                index={index}
-                isHoveredIndex={hoveredIndex} 
-                isClickedIndex={clickedIndex}
-                setHoveredIndex={setHoveredIndex} 
-                setClickedIndex={setClickedIndex}
-                />
-            )}
-            <Button
-            className="w-[20rem] h-[4rem] font-[BJG] mt-10 mb-10"
-            variant="outline"
-            onClick={(e) => handleLevelButtonClick(0, e)}
-            >
-            Back
-            </Button>
-        </div>
-    )
+  return (
+    <div className="text-center flex flex-col items-center">
+      {['0px', '-650px', '-650px'].map((marginTop, index) => (
+        <HoverFolder
+          marginTop={marginTop}
+          index={index}
+          isHoveredIndex={hoveredIndex}
+          isClickedIndex={clickedIndex}
+          setHoveredIndex={setHoveredIndex}
+          setClickedIndex={setClickedIndex}
+        />
+      ))}
+      <Button
+        className="w-[20rem] h-[4rem] font-[BJG] mt-10 mb-10"
+        variant="outline"
+        onClick={(e) => handleLevelButtonClick(0, e)}
+      >
+        Back
+      </Button>
+    </div>
+  );
 }
--- src/components/ui/HoverFolder.tsx
+++ src/components/ui/HoverFolder.tsx
@@ -1,60 +1,63 @@
-import folder_sprite from '../../assets/folder_sprite.png'
-import case_paper_sprite from '../../assets/case_paper_sprite.png'
-import { motion, AnimatePresence } from 'framer-motion'
+import folder_sprite from '../../assets/folder_sprite.png';
+import case_paper_sprite from '../../assets/case_paper_sprite.png';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface HoverFolderProps {
-  marginTop: string
-  index: number
-  isHoveredIndex: number
+  marginTop: string;
+  index: number;
+  isHoveredIndex: number;
   setHoveredIndex: (index: number) => void;
-  isClickedIndex: number
+  isClickedIndex: number;
   setClickedIndex: (index: number) => void;
 }
 
-export function HoverFolder({ 
-    marginTop, 
-    index, 
-    isHoveredIndex, 
-    setHoveredIndex, 
-    isClickedIndex,
-    setClickedIndex
+export function HoverFolder({
+  marginTop,
+  index,
+  isHoveredIndex,
+  setHoveredIndex,
+  isClickedIndex,
+  setClickedIndex
 }: HoverFolderProps) {
+  const isAboveHovered = index <= isHoveredIndex;
+  const isAboveClicked = index <= isClickedIndex;
 
-    const isAboveHovered = index <= isHoveredIndex
-    const isAboveClicked = index <= isClickedIndex
+  const hoverOffset = -10; // Distance moved by folder when hovered over
+  const clickedOffset = -200; // Distance moved by folder when clicked (selected)
 
-    const hoverOffset = -10; // Distance moved by folder when hovered over
-    const clickedOffset = -200; // Distance moved by folder when clicked (selected)
-
-    return (
-        <motion.div 
-        className="relative inline-block"
-        style={{ marginTop: marginTop}} // Adjust this value to control the initial vertical overlap
-        onMouseEnter={() => setHoveredIndex(index)}
-        onMouseLeave={() => setHoveredIndex(-1)} // Remove hover status
-        onClick={() => isClickedIndex === index ? setClickedIndex(-1) : setClickedIndex(index)}
-        animate={{ y: (isAboveClicked ? clickedOffset : 0) + (isAboveHovered ? hoverOffset : 0) }}
-        transition={{ type: "spring", stiffness: 1000, damping: 30 }}
-        >
-            <img
-            src={folder_sprite}
-            />
-            <AnimatePresence>
-                {isClickedIndex === index ? 
-                <motion.div
-                key={index}
-                initial={{ y: 1000, x: 20 }}
-                exit={{ y: 1000 }}
-                animate={{ y: index === isClickedIndex ? 50 : 1000 }} 
-                transition={{ type: "spring", duration: 0.8 }}
-                className="absolute inset-0 flex items-start justify-start text-2xl">
-                <img
-                src={case_paper_sprite}
-                />
-                </motion.div>
-                : <></>}
-            </AnimatePresence>
-        </motion.div>
-
-    );
+  return (
+    <motion.div
+      className="relative inline-block"
+      style={{ marginTop: marginTop }} // Adjust this value to control the initial vertical overlap
+      onMouseEnter={() => setHoveredIndex(index)}
+      onMouseLeave={() => setHoveredIndex(-1)} // Remove hover status
+      onClick={() =>
+        isClickedIndex === index ? setClickedIndex(-1) : setClickedIndex(index)
+      }
+      animate={{
+        y:
+          (isAboveClicked ? clickedOffset : 0) +
+          (isAboveHovered ? hoverOffset : 0)
+      }}
+      transition={{ type: 'spring', stiffness: 1000, damping: 30 }}
+    >
+      <img src={folder_sprite} />
+      <AnimatePresence>
+        {isClickedIndex === index ? (
+          <motion.div
+            key={index}
+            initial={{ y: '100vh', x: 20 }}
+            exit={{ y: '100vh' }}
+            animate={{ y: index === isClickedIndex ? 50 : 1000 }}
+            transition={{ type: 'spring', duration: 0.8 }}
+            className="absolute inset-0 flex items-start justify-start text-2xl"
+          >
+            <img src={case_paper_sprite} />
+          </motion.div>
+        ) : (
+          <></>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
 }
