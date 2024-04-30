9a5dbbd4da1c9c65445a2e911ef662ac2faace2d
rhun558 <rhun558@aucklanduni.ac.nz>
2024-04-30 14:28:07 +1200

    Created Sign In page. Added button to access it from main page. Copied most of new player page to make it as it is very similar. Is not yet functional, will need to connect to DB. Includes text fields, sign in button and back button.

---
+++ src/components/levels/SignIn.tsx
@@ -0,0 +1,63 @@
+import { Button } from '../ui/button';
+import { useState } from 'react';
+import { TextField } from '../ui/text_field';
+
+/* 
+This is the component for the new player menu. It allows the creation of a new player profile.
+So far it has input fields for a new profile name and password. These are not yet connected to a database.
+It also has a confirm button, which does nothing yet, and a back button to the landing page.
+*/
+
+interface SignInProps {
+  handleLevelButtonClick: (
+    level: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+  handleConfirm: (
+    username: string,
+    password: string,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+}
+
+export function SignIn({ handleLevelButtonClick, handleConfirm }: SignInProps) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <form>
+        <p>Identity:</p>
+        <TextField
+          value={username}
+          onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUsername(e.target.value)
+          }
+          placeholder={'Username'}
+        />
+        <p>Password:</p>
+        <TextField
+          value={password}
+          onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          placeholder={'Password'}
+        />
+      </form>
+      <Button
+        className="font-[alagard] text-[1.5rem] tracking-wide mt-2 w-[100%]"
+        onClick={(e) => handleConfirm(username, password, e)}
+        size={'sm'}
+      >
+        SIGN IN
+      </Button>
+      <Button
+        className="font-[alagard] text-[1.5rem] tracking-wide mt-2 w-[100%]"
+        onClick={(e) => handleLevelButtonClick(0, e)}
+        size={'sm'}
+      >
+        BACK
+      </Button>
+    </div>
+  );
+}
