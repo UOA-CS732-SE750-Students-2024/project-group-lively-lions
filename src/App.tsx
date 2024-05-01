851ff3b18c2f8b6e035fcf4a0911613a8af5b19f
jess-lowe <jlow337@aucklanduni.ac.nz>
2024-05-01 16:22:03 +1200

    feat: added a tutorial level

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { LevelSelect } from './components/levels/LevelSelect';
 import { NewPlayer } from './components/levels/NewPlayer';
 import { SignIn } from './components/levels/SignIn';
 import { PlayerInfo } from './components/levels/PlayerInfo';
+import { Tutorial } from './components/levels/Tutorial';
 
 function App() {
   const [currentLevel, setCurrentLevel] = useState(1);
@@ -24,7 +25,11 @@ function App() {
       handleLevelButtonClick={handleLevelButtonClick}
       handleConfirm={handleConfirm}
     />,
-    <LevelSelect handleLevelButtonClick={handleLevelButtonClick} />
+    <LevelSelect handleLevelButtonClick={handleLevelButtonClick} />,
+    <Tutorial
+      handleCheckAnswer={handleCheckAnswer}
+      handleLevelButtonClick={handleLevelButtonClick}
+    />
   ];
 
   function handleLevelButtonClick(
@@ -33,6 +38,19 @@ function App() {
   ) {
     e.preventDefault();
     setCurrentLevel(level);
+  }
+
+  function handleCheckAnswer(
+    userPhrase: string,
+    phrase: string,
+    e: React.MouseEvent<HTMLButtonElement>
+  ) {
+    e.preventDefault();
+    if (userPhrase == phrase) {
+      alert('Correct!');
+    } else {
+      alert('Incorrect, try again!');
+    }
   }
 
   function handleConfirm(
---
+++ src/components/levels/Tutorial.tsx
@@ -0,0 +1,66 @@
+import { Button } from '../ui/button';
+import { useState } from 'react';
+import { TextField } from '../ui/text_field';
+import { SimpleCaesarCipher } from '@/ciphers';
+
+/* 
+This is the component for the sign in menu. It allows the user to sign into their profile.
+So far it has input fields for a profile name and password. These are not yet connected to a database.
+It also has a confirm button, which does nothing yet, and a back button to the landing page.
+*/
+
+interface TutorialProps {
+  handleCheckAnswer: (
+    userPhrase: string,
+    phrase: string,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+
+  handleLevelButtonClick: (
+    level: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+}
+
+export function Tutorial({
+  handleCheckAnswer,
+  handleLevelButtonClick
+}: TutorialProps) {
+  const phrase = 'Surprise! Happy Birthday Detective Purrlock Holmes!';
+  const [userPhrase, setUserPhrase] = useState('');
+  return (
+    <div>
+      <p className="text-white">
+        The Case of the Missing Milk: Purrlock Holmes receives a frantic call
+        from Mrs. Calico. Her prized saucer of milk has vanished! The only clue
+        is a strange note left behind with scrambled letters. Purrlock must use
+        his knowledge of the Caesar Cipher to decipher the note and identify the
+        milk thief.
+      </p>
+      <SimpleCaesarCipher caesarkey={3} phrase={phrase} />
+
+      <form>
+        <TextField
+          value={userPhrase}
+          onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUserPhrase(e.target.value)
+          }
+          placeholder={'Answer'}
+        />
+      </form>
+      <Button
+        className="font-[alagard] text-[1.5rem] tracking-wide mt-2 w-[100%]"
+        onClick={(e) => handleCheckAnswer(userPhrase, phrase, e)}
+        size={'sm'}
+      >
+        Check
+      </Button>
+      <Button
+        className="font-[alagard] text-[1rem] mt-5 mb-5 bottom-0"
+        onClick={(e) => handleLevelButtonClick(0, e)}
+      >
+        Back
+      </Button>
+    </div>
+  );
+}
