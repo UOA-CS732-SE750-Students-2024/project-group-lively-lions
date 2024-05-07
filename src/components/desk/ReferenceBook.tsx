91f7bd30fa2a236c26ed0c1c8d92c777e8cbd06b
rhun558 <rhun558@aucklanduni.ac.nz>
2024-05-07 16:52:00 +1200

    Created cipherInfo with some sample cipher data for testing. Added reference book page tsx file to display each cipher info. Added image of page to put the text over. Properly implemented reference book to use two pages and display them side by side, giving them the correct cipher data.

--- src/components/desk/ReferenceBook.tsx
+++ src/components/desk/ReferenceBook.tsx
@@ -1,5 +1,8 @@
 import { Screen } from '@/util';
 import { Button } from '../ui/button';
+import { ReferenceBookPage } from './ReferenceBookPage';
+import cipherInfo from '../../lib/cipherInfo.json';
+import { useState } from 'react';
 
 /* 
 This is the component for the reference book. This will need to be updated.
@@ -13,15 +16,91 @@ interface ReferenceBookProps {
 }
 
 export function ReferenceBook({ handleScreenButtonClick }: ReferenceBookProps) {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageChange = (increment: number): void => {
+    if (
+      currentPage + increment < cipherInfo.cipherList.length &&
+      currentPage + increment >= 0
+    ) {
+      setCurrentPage(currentPage + increment);
+    }
+  };
+
+  const handleGetName = (increment: number): string => {
+    if (currentPage + increment < cipherInfo.cipherList.length) {
+      return cipherInfo.cipherList[currentPage + increment].name;
+    } else {
+      return '';
+    }
+  };
+
+  const handleGetEncoding = (increment: number): string => {
+    if (currentPage + increment < cipherInfo.cipherList.length) {
+      return cipherInfo.cipherList[currentPage + increment].encoding;
+    } else {
+      return '';
+    }
+  };
+
+  const handleGetDecoding = (increment: number): string => {
+    if (currentPage + increment < cipherInfo.cipherList.length) {
+      return cipherInfo.cipherList[currentPage + increment].decoding;
+    } else {
+      return '';
+    }
+  };
+
   return (
     <div>
-      <p>REFERENCE BOOK</p>
+      <div className="flow-root">
+        <p
+          className="
+      relative bg-[#dde7e9] rounded-md
+      w-[calc(30vw)] h-[calc(45vw*9/16)]
+      min-w-[425px] min-h-[337.5px]
+      overflow-scroll no-scrollbar float-left"
+        >
+          <ReferenceBookPage
+            cipherName={handleGetName(0)}
+            encoding={handleGetEncoding(0)}
+            decoding={handleGetDecoding(0)}
+          />
+        </p>
+        <p
+          className="
+      relative bg-[#dde7e9] rounded-md
+      w-[calc(30vw)] h-[calc(45vw*9/16)]
+      min-w-[425px] min-h-[337.5px]
+      overflow-scroll no-scrollbar float-right"
+        >
+          <ReferenceBookPage
+            cipherName={handleGetName(1)}
+            encoding={handleGetEncoding(1)}
+            decoding={handleGetDecoding(1)}
+          />
+        </p>
+      </div>
+      <Button
+        className="font-[alagard] text-[1.5rem] tracking-wide mt-2 w-[100%]"
+        onClick={() => handlePageChange(2)}
+        size={'sm'}
+      >
+        NEXT
+      </Button>
+      <Button
+        className="font-[alagard] text-[1.5rem] tracking-wide mt-2 w-[100%]"
+        onClick={() => handlePageChange(-2)}
+        size={'sm'}
+      >
+        BACK
+      </Button>
       <Button
         className="font-[alagard] text-[1.5rem] tracking-wide mt-2 w-[100%]"
         onClick={(e) => handleScreenButtonClick(Screen.MainMenuScreen, e)}
         size={'sm'}
       >
-        BACK
+        EXIT
       </Button>
     </div>
   );
