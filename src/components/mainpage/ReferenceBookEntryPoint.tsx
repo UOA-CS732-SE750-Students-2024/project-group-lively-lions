084bb86a1e695b86bd0241d66b86e9980532db3a
Gauri <gauriraj.nz@gmail.com>
2024-05-08 22:20:39 +1200

    Added reference book sound effects

--- src/components/mainpage/ReferenceBookEntryPoint.tsx
+++ src/components/mainpage/ReferenceBookEntryPoint.tsx
@@ -1,5 +1,8 @@
 import refBook from '../../assets/room/shared/reference_book.png';
 import { Screen } from '@/util';
+import { motion } from 'framer-motion';
+import bookHoverSound from '../../assets/sounds/book_slide.mp4';
+import bookOpenSound from '../../assets/sounds/book_open.mp4';
 
 interface ReferenceBookEntryPointProps {
     handleScreenButtonClick: (
@@ -12,10 +15,22 @@ export default function ReferenceBookEntryPoint({
     handleScreenButtonClick
 }: ReferenceBookEntryPointProps) {
 
+    function playBookHoverSound() {
+        new Audio(bookHoverSound).play();
+    }
+
+    function playBookOpenSound() {
+        new Audio(bookOpenSound).play();
+    }
+
     return (
         <div>
-            <button onClick={(e) => handleScreenButtonClick(Screen.ReferenceBook, e)}>
-                <img className=' hover:animate-book-shuffle' src={refBook} alt='book' />
+            <button onClick={(e) => { handleScreenButtonClick(Screen.ReferenceBook, e); playBookOpenSound() }}>
+                <motion.img
+                    className=' hover:animate-book-shuffle'
+                    onMouseOver={() => playBookHoverSound()}
+                    src={refBook}
+                    alt='book' />
             </button>
         </div>
     )
