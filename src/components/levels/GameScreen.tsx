eb548bf5d7d0eaae84a593b5fba14d68e18fd866
Matt Stevens <m.c.stevens74@gmail.com>
2024-05-09 21:19:28 +1200

    Added explanation for my sin.

--- src/components/levels/GameScreen.tsx
+++ src/components/levels/GameScreen.tsx
@@ -258,6 +258,7 @@ export default function GameScreen({
       />
 
       {/* Reference book */}
+      {/* This asset just looks better when on top of the hard pixel vignette, sorry for throwing off the nice grouping. */}
       <div className="absolute scale-[250%] top-[71%] left-[7%] rotate-12">
         <ReferenceBook isMuted={isMuted} />
       </div>
